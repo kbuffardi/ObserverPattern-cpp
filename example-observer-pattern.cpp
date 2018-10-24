529c4885c9dbb71a0340001caa6cffd1e60ed111
Kevin Buffardi <kbuffardi@csuchico.edu>
2018-10-23 19:34:58 -0700

    Update example for pseudo-MVC with 2 controllers

--- example-observer-pattern.cpp
+++ example-observer-pattern.cpp
@@ -27,40 +27,78 @@ public:
   }
 };
 
-
-// controller that 
-class IOController: public IObserver
+class Alert
 {
-private:
-  DataModel current;
-  Logger output;
-
 public:
-  IOController()
+  void sendAlert(const int number, const char* alert)
   {
-    current.addObserver(this);
-  }
-  void input(int x)
-  {
-    current.setNumber(x);
-  }
-  void update()
-  {
-    output.showNumber(current.getNumber());
+    std::cout<<"ALERT! "<<number<<" is "<<alert<<std::endl;
   }
 };
 
 
+// controller that gathers input to change data model and drives log updates
+class IOController: public IObserver
+{
+private:
+  DataModel* current;
+  Logger output;
+
+public:
+  IOController(DataModel* data)
+  {
+    current = data;
+    current->addObserver(this);
+  }
+  void input(int x)
+  {
+    current->setNumber(x);
+  }
+  void update()
+  {
+    output.showNumber(current->getNumber());
+  }
+};
+
+//controller that displays alerts under certain conditions
+class AlertController: public IObserver
+{
+private:
+  DataModel* dataState;
+  Alert display;
+
+public:
+  AlertController(DataModel* data)
+  {
+    dataState = data;
+    dataState->addObserver(this);
+  }
+  void update()
+  {
+    int num = dataState->getNumber();
+    if( num < 0 )
+    {
+      display.sendAlert(num,"negative");
+    }
+    else if( num == 0 )
+    {
+      display.sendAlert(num,"zero");
+    }
+  }
+};
+
+//Program run
 int main()
 {
-  IOController example;
-  ErrorLogController 
+  DataModel persistant;
+  IOController io(&persistant);
+  AlertController notifications(&persistant);
 
   // simulate the user's actions that change state
-  example.input(100);
-  example.input(10);
-  example.input(-1);
-  example.input(0);
+  io.input(100);
+  io.input(10);
+  io.input(-1);
+  io.input(0);
 
   return 0;
 }
